07ef1474ce5c9ed31325d8aafe0f4775ffb17fc3
santhoshkn011 <santhoshkn011@gmail.com>
2022-12-08 14:35:13 +0530

    Print Integers

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # 100_JavaPrograms
 1. Simple Java Program
+2. Print Integers
---
+++ src/main/java/org/example/PrintIntegers.java
@@ -0,0 +1,9 @@
+package org.example;
+//Printing integers 0 to 9
+public class PrintIntegers {
+    public static void main(String[] args) {
+        for(int i=0; i<10; i++){
+            System.out.println(i);
+        }
+    }
+}
